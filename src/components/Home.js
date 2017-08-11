c059018ab2f682b7bb05eba7f992d3beaea3028c
Amanda Joan <amandajoanski@gmail.com>
2017-08-11 09:29:20 -0700

    added styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends React.Component {
     currentEvent: null
   }
 
-
+//brings in react route from auth file and sets state of events
   componentDidMount() {
     auth.getEvents().then(events => {
       this.setState({events:events})
@@ -21,54 +21,72 @@ class Home extends React.Component {
 
   clickHandle(id){
     this.setState({currentEvent: null})
-    auth.getEvent(id).then(event => {
+      auth.getEvent(id).then(event => {
       this.setState({currentEvent: event})
       console.log(this.state.currentEvent);
       })
+    document.getElementById('userDiv').style.display="block";
   }
+
+
 
   render() {
     const
       currentUser = this.props.currentUser
+      var currentEvent = this.state.currentEvent
       var eId =''
 
     return (
 
-      <div>
-        {currentUser
-            ? <p>Current User: {currentUser.name}</p>
-            : null
-          }
-        <h1>Events</h1>
+  <div>
+    <div id="header">
+      <img id="logo" src="../logo.png"/>
+    </div>
 
+      <div className="container">
+        <div className="row">
+
+          <div className="col-sm-6">
+            <h2>Events</h2>
             <div>
               <ul id="eventsList">
-
                 {this.state.events.map(event => {
                 eId = event._id
-                return <li key={event._id} onClick={this.clickHandle.bind(this, event._id)}>{event.title}</li>
+                return <li key={event._id} onClick={this.clickHandle.bind(this, event._id)}><h4> + {event.title}, {event.location}</h4></li>
                 })}
               </ul>
             </div>
+          </div>
+          <div className="col-sm-6">
+            <h2>Selected Event</h2>
+            <div>
+                {this.state.currentEvent ?
+                  <Event event={currentEvent}/>
+                  :
+                  null
+                }
+            </div>
+          </div>
 
-            <div id="container">
-
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <div id="newEventContainer">
                 <ShowHide currentUser={currentUser} />
-
-
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div id="userDiv">
+              <img src="../usersPic.png"/>
             </div>
 
-
-        <div>
-          {this.state.currentEvent ?
-            <Event event={this.state.currentEvent}/>
-            :
-            null
-          }
+          </div>
 
         </div>
-
-        </div>
+      </div>
+</div>
 
     )
   }
